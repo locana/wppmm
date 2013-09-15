3432a7b26e890158e28c4cc198722430d799d19e
opelzz <m.opelzz@gmail.com>
2013-09-16 02:17:38 +0900

    Divide test code

--- WPPMM/WPPMM/Pages/MainPage.xaml.cs
+++ WPPMM/WPPMM/Pages/MainPage.xaml.cs
@@ -17,55 +17,8 @@ namespace WPPMM
             // ApplicationBar をローカライズするためのサンプル コード
             //BuildLocalizedApplicationBar();
 
-            string req = Request.actTakePicture();
-            Debug.WriteLine(req);
-
-            req = Request.actZoom("in", "1shot");
-            Debug.WriteLine(req);
-
-            req = Request.setSelfTimer(1);
-            Debug.WriteLine(req);
-
-            string json = "{\"result\": [[\"http://ip:port/postview/postview.jpg\"]],\"id\": 1}";
-
-            ResultHandler.ActTakePicture(json, HandleError, HandleActTakePictureResult);
-
-            DeviceDiscovery.SearchScalarDevices(10, HandleDDLocation, HandleSsdpError);
         }
 
-        private void HandleError(int code)
-        {
-            Debug.WriteLine("Error: " + code);
-        }
-
-        private void HandleActTakePictureResult(string[] urls)
-        {
-            Debug.WriteLine("HandleActTakePictureResult");
-            foreach (var url in urls)
-            {
-                Debug.WriteLine("URL: " + url);
-            }
-        }
-
-        private void HandleDDLocation(string dd_url)
-        {
-            Debug.WriteLine("handle dd location: " + dd_url);
-            DeviceDiscovery.RetrieveEndpoints(dd_url, HandleEndpoints, HandleSsdpError);
-        }
-
-        private void HandleSsdpError()
-        {
-            Debug.WriteLine("handle ssdp error");
-        }
-
-        private void HandleEndpoints(Dictionary<string, string> endpoints)
-        {
-            Debug.WriteLine("handle endpoints");
-            foreach (var service in endpoints.Keys)
-            {
-                Debug.WriteLine("Endpoint of " + service + ": " + endpoints[service]);
-            }
-        }
 
         // ローカライズされた ApplicationBar を作成するためのサンプル コード
         //private void BuildLocalizedApplicationBar()

e8b631996e306fba3d4645cd2cf2bb53861fdbbc
Nao Taco <naotaco@gmail.com>
2013-09-16 13:13:51 +0900

    add English to supported language.

---
+++ WPPMM/WPPMM/CameraManager/SearchDevices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPPMM.CameraManager
+{
+    class SearchDevices
+    {
+
+        // Singleton
+        private static SearchDevices searchDevices = new SearchDevices();
+
+        private static int TIMEOUT = 30;
+
+        private SearchDevices()
+        {
+            RequestSearchDevices();
+        }
+
+        public static SearchDevices GetInstance()
+        {
+            return searchDevices;
+        }
+
+
+        public static void RequestSearchDevices()
+        {
+            WPPMM.Ssdp.DeviceDiscovery.SearchScalarDevices(TIMEOUT, OnDDLocationFound, OnTimeout);
+        }
+
+        public static void OnDDLocationFound(String location)
+        {
+            Debug.WriteLine("found location: " + location);
+        }
+
+        private static void OnTimeout()
+        {
+            Debug.WriteLine("request timeout.");
+        }
+    }
+}

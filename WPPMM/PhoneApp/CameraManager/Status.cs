39debfb22f63bc462e901bafb05a220bd5b73e0d
opelzz <m.opelzz@gmail.com>
2013-10-26 13:24:13 +0900

    Modify folder structure

---
+++ WPPMM/PhoneApp/CameraManager/Status.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using WPPMM.RemoteApi;
+
+namespace WPPMM.CameraManager
+{
+
+    public class Status
+    {
+
+        /// <summary>
+        /// returnes true if it's possible to connect.
+        /// (device info has got correctly)
+        /// </summary>
+        public bool isAvailableConnecting
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Is this phone connected to target device (after getting URL of liveview)
+        /// </summary>
+        public bool isConnected
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Is available shooting (liveview running)
+        /// </summary>
+        public bool isAvailableShooting
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// true during taking picture
+        /// </summary>
+        public bool isTakingPicture
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// returnes true during rendering jpeg image on live view screen
+        /// </summary>
+        public bool isRendering
+        {
+            get;
+            set;
+        }
+
+        public List<String> MethodTypes
+        {
+            get;
+            set;
+        }
+
+        public void Init()
+        {
+            _init();
+        }
+
+        public Status()
+        {
+            _init();
+        }
+
+        private void _init()
+        {
+            isAvailableConnecting = false;
+            isAvailableShooting = false;
+            isConnected = false;
+            isTakingPicture = false;
+            isRendering = false;
+            MethodTypes = new List<string>();
+        }
+
+        public string[] AvailableApis { set; get; }
+        public string CameraStatus { set; get; }
+        public ZoomInfo ZoomInfo { set; get; }
+        public bool LiveviewAvailable { set; get; }
+        public BasicInfo<string> PostviewSizeInfo { set; get; }
+        public BasicInfo<int> SelfTimerInfo { set; get; }
+        public BasicInfo<string> ShootModeInfo { set; get; }
+        public BasicInfo<string> ExposureMode { set; get; }
+        public BasicInfo<string> ShutterSpeed { set; get; }
+        public BasicInfo<string> ISOSpeedRate { set; get; }
+        public BasicInfo<string> FNumber { set; get; }
+        public EvInfo EvInfo { set; get; }
+        public bool ProgramShiftActivated { set; get; }
+    }
+}
---
+++ WPPMM/PhoneApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,185 @@
+﻿using Microsoft.Phone.Controls;
+using Microsoft.Phone.Net.NetworkInformation;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+using System;
+using System.Diagnostics;
+using System.Windows.Navigation;
+using WPPMM.CameraManager;
+using WPPMM.Resources;
+
+
+namespace WPPMM
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        private static CameraManager.CameraManager cameraManager;
+
+        // コンストラクター
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BuildLocalizedApplicationBar();
+
+            cameraManager = CameraManager.CameraManager.GetInstance();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Debug.WriteLine(e.Uri);
+            progress.IsVisible = false;
+            cameraManager.Refresh();
+            UpdateNetworkStatus();
+            IsReadyToControl = false;
+            if (GetSSIDName().StartsWith("DIRECT-"))
+            {
+                StartConnectionSequence(NavigationMode.New == e.NavigationMode);
+            }
+        }
+
+        private bool SupressPageMove = false;
+
+        private bool IsReadyToControl = false;
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            SupressPageMove = true;
+        }
+
+        private void StartConnectionSequence(bool connect)
+        {
+            progress.IsVisible = connect;
+            SupressPageMove = false;
+            CameraManager.CameraManager.GetInstance().RequestSearchDevices(() =>
+            {
+                IsReadyToControl = true;
+                if (connect && !SupressPageMove) GoToShootingPage();
+            }, () =>
+            {
+                progress.IsVisible = false;
+            });
+        }
+
+        private void HandleError(int code)
+        {
+            Debug.WriteLine("Error: " + code);
+        }
+
+        private void HandleActTakePictureResult(string[] urls)
+        {
+            Debug.WriteLine("HandleActTakePictureResult");
+            foreach (var url in urls)
+            {
+                Debug.WriteLine("URL: " + url);
+            }
+        }
+
+        private void UpdateNetworkStatus()
+        {
+            var ssid = GetSSIDName();
+            Debug.WriteLine("SSID: " + ssid);
+            if (DeviceNetworkInformation.IsWiFiEnabled)
+            {
+                NetworkStatus.Text = AppResources.Guide_WiFiNotEnabled;
+                StartRemoteButton.IsEnabled = true;
+            }
+            else if (ssid.StartsWith("DIRECT-"))
+            {
+                NetworkStatus.Text = AppResources.Guide_CantFindDevice;
+            }
+            else
+            {
+                NetworkStatus.Text = AppResources.Guide_WiFiNotConnected;
+            }
+
+            if (cameraManager.GetDeviceInfo() != null)
+            {
+                String modelName = cameraManager.GetDeviceInfo().FriendlyName;
+                if (modelName != null)
+                {
+                    NetworkStatus.Text = "Connected device: " + modelName;
+                }
+            }
+
+            // display initialize
+
+            ProgressBar.Visibility = System.Windows.Visibility.Collapsed;
+            cameraManager.UpdateEvent += WifiUpdateListener;
+        }
+
+        private void OnWiFiSettingButtonClicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ConnectionSettingsTask connectionSettingsTask = new ConnectionSettingsTask();
+            connectionSettingsTask.ConnectionSettingsType = ConnectionSettingsType.WiFi;
+            connectionSettingsTask.Show();
+        }
+
+        private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (IsReadyToControl)
+            {
+                GoToShootingPage();
+            }
+            else
+            {
+                StartConnectionSequence(true);
+                ProgressBar.IsIndeterminate = true;
+            }
+        }
+
+        private void GoToShootingPage()
+        {
+            NavigationService.Navigate(new Uri("/Pages/LiveViewScreen.xaml", UriKind.Relative));
+        }
+
+        internal void WifiUpdateListener(Status cameraStatus)
+        {
+
+            if (cameraStatus.isAvailableConnecting)
+            {
+                String modelName = cameraManager.GetDeviceInfo().FriendlyName;
+                NetworkStatus.Text = "Connected device: " + modelName;
+            }
+
+            if (cameraStatus.isAvailableConnecting && cameraStatus.MethodTypes != null)
+            {
+                StartRemoteButton.IsEnabled = true;
+            }
+
+        }
+
+        private string GetSSIDName()
+        {
+            foreach (var network in new NetworkInterfaceList())
+            {
+                if (
+                    (network.InterfaceType == NetworkInterfaceType.Wireless80211) &&
+                    (network.InterfaceState == ConnectState.Connected)
+                    )
+                {
+                    return network.InterfaceName;
+                }
+            }
+            return "<Not connected>";
+        }
+
+        private void OSS_Menu_Click(object sender, System.EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Pages/LicensePage.xaml", UriKind.Relative));
+        }
+
+        private void BuildLocalizedApplicationBar()
+        {
+            ApplicationBar = new ApplicationBar();
+            ApplicationBar.Mode = ApplicationBarMode.Minimized;
+            ApplicationBar.Opacity = 0.5;
+
+            var OssMenuItem = new ApplicationBarMenuItem(AppResources.OSSText);
+            OssMenuItem.Click += OSS_Menu_Click;
+            ApplicationBar.MenuItems.Add(OssMenuItem);
+        }
+    }
+}

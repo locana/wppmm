2aebbeefa0ad2d88523ef2c6258fcebd6c340f87
kazyx <dev.ykz@gmail.com>
2013-11-09 02:06:25 +0900

    Update version number

--- WPPMM/PhoneApp/Properties/AssemblyInfo.cs
+++ WPPMM/PhoneApp/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // すべての値を指定するか、次のように '*' を使ってリビジョンおよびビルド番号を 
 // 既定値にすることができます。:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: NeutralResourcesLanguageAttribute("ja-JP")]

6bfed41492013b7a9dab7802c85839232b552c82
Nao Taco <naotaco@gmail.com>
2014-04-30 00:54:15 +0900

    Operate EV by the dial

--- Project/Controls/SettingDial.xaml.cs
+++ Project/Controls/SettingDial.xaml.cs
@@ -10,21 +10,28 @@ using Microsoft.Phone.Shell;
 using System.Diagnostics;
 using System.Windows.Media.Animation;
 using System.Windows.Media;
+using Kazyx.WPPMM.CameraManager;
 
 namespace Kazyx.WPMMM.Controls
 {
     public partial class SettingDial : UserControl
     {
-        public const int THRESHOLD = 20;
+        public const int THRESHOLD = 7;
         public const int GEAR_SPEED = 10;
 
         private double GearAngle;
+        private int OperationCount;
+
+        private CameraManager manager;
 
         public SettingDial()
         {
             InitializeComponent();
 
             GearAngle = 0;
+            OperationCount = 0;
+
+            manager = CameraManager.GetInstance();
         }
 
         private void Gear_ManipulationDelta(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
@@ -40,18 +47,37 @@ namespace Kazyx.WPMMM.Controls
                 Debug.WriteLine("Operation: Down");
                 RotateGear(GearAngle, GearAngle - GEAR_SPEED);
                 GearAngle -= GEAR_SPEED;
+                OperationCount--;
             }
             else if (delta.Translation.Y < (-THRESHOLD))
             {
                 Debug.WriteLine("Operation: Up");
                 RotateGear(GearAngle, GearAngle + THRESHOLD);
                 GearAngle += GEAR_SPEED;
+                OperationCount++;
             }
         }
 
         private void Gear_ManipulationCompleted(object sender, System.Windows.Input.ManipulationCompletedEventArgs e)
         {
+            if (manager == null || manager.cameraStatus == null)
+            {
+                OperationCount = 0;
+                return;
+            }
 
+            // Ev
+            if (manager.cameraStatus.IsAvailable("setExposureCompensation") && manager.cameraStatus.EvInfo != null)
+            {
+                var target = manager.cameraStatus.EvInfo.CurrentIndex + OperationCount;
+                if (target >= manager.cameraStatus.EvInfo.Candidate.MinIndex && target <= manager.cameraStatus.EvInfo.Candidate.MaxIndex)
+                {
+                    manager.SetExposureCompensation(target);
+                }
+            }
+
+
+            OperationCount = 0;
         }
 
         private void RotateGear(double from, double to)

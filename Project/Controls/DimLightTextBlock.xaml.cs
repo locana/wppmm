762d5380ab3c76ca24bf04fb99a201bca9c5b1d8
kazyx <dev.ykz@gmail.com>
2014-05-06 02:22:34 +0900

    DimLightTextBlock proto (not working)

---
+++ Project/Controls/DimLightTextBlock.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Kazyx.WPMMM.Controls
+{
+    public partial class DimLightTextBlock : UserControl
+    {
+        public event EventHandler AnimationCompleted;
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+            "Title",
+            typeof(string),
+            typeof(DimLightTextBlock),
+            new PropertyMetadata(
+                new PropertyChangedCallback(OnTitleChanged)
+                )
+            );
+
+        private static void OnTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var dim = d as DimLightTextBlock;
+            var title = e.NewValue as string;
+            dim.MainText.Text = title;
+        }
+
+        public string Title
+        {
+            set { SetValue(TitleProperty, value); }
+            get { return GetValue(TitleProperty) as string; }
+        }
+
+        protected void OnFinished()
+        {
+            if (AnimationCompleted != null)
+            {
+                AnimationCompleted(this, new EventArgs());
+            }
+        }
+
+        public DimLightTextBlock()
+        {
+            InitializeComponent();
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            OnFinished();
+        }
+    }
+}

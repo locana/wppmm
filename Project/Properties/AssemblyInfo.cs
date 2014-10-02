a661791da731f48133f27d75d6fde02b6471de45
kazyx <dev.ykz@gmail.com>
2014-10-03 00:16:30 +0900

    Increment version number

--- Project/Properties/AssemblyInfo.cs
+++ Project/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("a4e5a164-16db-4263-94ce-bc98bef138c7")]
 
-[assembly: AssemblyVersion("2.6.2")]
-[assembly: AssemblyFileVersion("2.6.2")]
+[assembly: AssemblyVersion("2.7.0")]
+[assembly: AssemblyFileVersion("2.7.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]

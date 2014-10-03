52f7e4865b3b347bec3df73733f32bed5f5bde69
Nao Taco <naotaco@gmail.com>
2014-10-04 02:48:03 +0900

    Update focusframe layout

--- Project/Controls/FocusFrames.xaml.cs
+++ Project/Controls/FocusFrames.xaml.cs
@@ -30,8 +30,11 @@ namespace Kazyx.WPPMM.Controls
         private static readonly Brush NormalBrush = (Brush)Application.Current.Resources["PhoneBackgroundBrush"];
         private static readonly Brush MainBrush = (Brush)Application.Current.Resources["PhoneForegroundBrush"];
         private static readonly Brush SubBrush = (Brush)Application.Current.Resources["PhoneBackgroundBrush"];
+        private static readonly double FaceFrameStrokeThickness = 1.5;
+        private static readonly double FrameStrokeThickness = 2.5;
+        private static readonly double FrameOpacity = 0.7;
 
-        void AddFrame(double X1, double X2, double Y1, double Y2, Brush StrokeBrush, bool DottedBorder = false)
+        void AddFrame(double X1, double X2, double Y1, double Y2, Brush StrokeBrush, double StrokeThickness, bool DottedBorder = false)
         {
             DebugUtil.Log("[FocusFrames] " + X1 + " " + X2 + " " + Y1 + " " + Y2);
             var r = new Rectangle()
@@ -42,7 +45,8 @@ namespace Kazyx.WPPMM.Controls
                 Height = Y2 - Y1,
                 Margin = new Thickness(X1, Y1, 0, 0),
                 Stroke = StrokeBrush,
-                StrokeThickness = 1,
+                Opacity = FrameOpacity,
+                StrokeThickness = StrokeThickness,
             };
 
             if (DottedBorder)
@@ -50,7 +54,6 @@ namespace Kazyx.WPPMM.Controls
                 var d = new DoubleCollection();
                 d.Add(4);
                 r.StrokeDashArray = d;
-                r.StrokeThickness = 1.5;
             }
 
             this.LayoutRoot.Children.Add(r);
@@ -67,37 +70,38 @@ namespace Kazyx.WPPMM.Controls
                 double y1 = (double)info.TopLeft_Y / 10000 * LayoutRoot.ActualHeight;
                 double y2 = (double)info.BottomRight_Y / 10000 * LayoutRoot.ActualHeight;
 
-                // DebugUtil.Log("FocusFrame update" + x1 + " " + x2 + " " + y1 + " " + y2);
-
+                var brush = NormalBrush;
                 switch (info.Category)
                 {
                     case Category.ContrastAF:
                     case Category.Tracking:
                     case Category.PhaseDetectionAF:
+                        
                         switch (info.Status)
                         {
                             case Status.Focused:
-                                this.AddFrame(x1, x2, y1, y2, FocusedBrush, info.AdditionalStatus == AdditionalStatus.LargeFrame);
+                                brush = FocusedBrush;
                                 break;
                             case Status.Normal:
-                                this.AddFrame(x1, x2, y1, y2, NormalBrush, info.AdditionalStatus == AdditionalStatus.LargeFrame);
+                                brush = NormalBrush;
                                 break;
-
                         }
+                        this.AddFrame(x1, x2, y1, y2, brush, FrameStrokeThickness, info.AdditionalStatus == AdditionalStatus.LargeFrame);
                         break;
                     case Category.Face:
                         switch (info.Status)
                         {
                             case Status.Focused:
-                                this.AddFrame(x1, x2, y1, y2, FocusedBrush);
+                                brush = FocusedBrush;
                                 break;
                             case Status.Main:
-                                this.AddFrame(x1, x2, y1, y2, MainBrush);
+                                brush = MainBrush;
                                 break;
                             case Status.Sub:
-                                this.AddFrame(x1, x2, y1, y2, SubBrush);
+                                brush = SubBrush;
                                 break;
                         }
+                        this.AddFrame(x1, x2, y1, y2, brush, FaceFrameStrokeThickness);
                         break;
                 }
             }
